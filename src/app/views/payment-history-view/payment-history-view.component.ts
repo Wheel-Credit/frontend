583736136bbf4e3048151892d5cf054546eb8be3
lucas1336 <lucasruiz1336@gmail.com>
2023-11-20 21:21:05 -0500

    feat: added footer and header

--- src/app/views/payment-history-view/payment-history-view.component.ts
+++ src/app/views/payment-history-view/payment-history-view.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-payment-history-view',
   templateUrl: './payment-history-view.component.html',
-  styleUrls: ['./payment-history-view.component.css']
+  styleUrls: ['./payment-history-view.component.css'],
 })
 export class PaymentHistoryViewComponent {
+  valideUser: boolean = true;
 
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.userService.getAuth().subscribe(
+      (response) => {
+        if (response) {
+          this.valideUser = true;
+        } else {
+          this.valideUser = false;
+        }
+      },
+      (error) => {
+        this.valideUser = false;
+      }
+    );
+  }
+
+  redirectToSignIn() {
+    this.router.navigate(['/auth']);
+  }
 }
